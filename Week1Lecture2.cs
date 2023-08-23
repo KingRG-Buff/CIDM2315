5a7519644fa16024f556a6f9911bf9a9bbed9f5a
Raul G. Garcia III <142825754+KingRG-Buff@users.noreply.github.com>
2023-08-23 11:57:57 -0500

    Add files via upload
    Lecture from week one, Uploading file from lecture work conducted in class.

---
+++ Week1Lecture2.cs
@@ -0,0 +1,11 @@
+﻿namespace Week1Lecture2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        int b = a++;
+        Console.WriteLine("a: " +a); //
+        Console.WriteLine("b: "+b); //
+    }
+}
