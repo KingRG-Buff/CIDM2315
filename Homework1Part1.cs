4b02ae0d2b009431a98ef158aaec19aca3e02202
Raul G. Garcia III <142825754+KingRG-Buff@users.noreply.github.com>
2023-08-28 11:33:14 -0500

    Add files via upload
    Week1 homework assignment submission for part 1

---
+++ Homework1Part1.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWork1Part1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = 4 * X * X + 3 * Y;
+
+        Console.Write($"X = {X}, ");
+        Console.WriteLine($"Y = {Y}");
+        Console.WriteLine($"The value of Z is: {Z}");
+    }
+}
