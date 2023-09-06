a40c44cf67d35172e576c2538fcc2ab8a09ccb5d
Raul G. Garcia III <142825754+KingRG-Buff@users.noreply.github.com>
2023-09-05 23:46:10 -0500

    Add files via upload
    Week 2 Homework Assignment

---
+++ Week2Homework.cs
@@ -0,0 +1,67 @@
+﻿namespace Week2Homework;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Question 1 Code
+        /*Console.WriteLine("Please input a letter grade:");
+        string grade = Console.ReadLine();
+
+        int gpaPoints;
+
+        if (grade == "A")
+        {
+            gpaPoints = 4;
+        }
+        else if (grade == "B")
+        {
+            gpaPoints = 3;
+        }
+        else if (grade == "C")
+        {
+            gpaPoints = 2;
+        }
+        else if (grade == "D")
+        {
+            gpaPoints = 1;
+        }
+        else if (grade == "F")
+        {
+            gpaPoints = 0;
+        }
+        else
+        {
+            Console.WriteLine("Wrong Letter Grade!");
+            return;
+        }
+
+        Console.WriteLine("GPA point: " + gpaPoints);*/
+        
+        //Question 2 code
+        Console.WriteLine("Please input the first num:");
+        string input1 = Console.ReadLine();
+        short num1 = Convert.ToInt16(input1);
+
+        Console.WriteLine("Please input the second num:");
+        string input2 = Console.ReadLine();
+        short num2 = Convert.ToInt16(input2);
+
+        Console.WriteLine("Please input the third num:");
+        string input3 = Console.ReadLine();
+        short num3 = Convert.ToInt16(input3);
+
+        short smallest = num1;
+
+        if (num2 < smallest)
+        {
+            smallest = num2;
+        }
+
+        if (num3 < smallest)
+        {
+            smallest = num3;
+        }
+
+        Console.WriteLine("The smallest value is: " + smallest);
+    }
+}
