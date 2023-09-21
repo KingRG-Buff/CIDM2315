cb101ee24f0291761593a8e0fa34b255ad06e60e
Raul G. Garcia III <142825754+KingRG-Buff@users.noreply.github.com>
2023-09-20 23:29:08 -0500

    Add files via upload
    Week 4 Homework File Upload

---
+++ Wk4Homework.cs
@@ -0,0 +1,35 @@
+﻿namespace Wk4HomeWork;
+class Program
+{
+
+
+    // Homework Question 1
+    public static int numbers(int a, int b)
+
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+    static void Main(string[] args)   
+    {
+       Console.WriteLine("Enter a number:");
+            int a = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("Enter another number:");
+            int b = Convert.ToInt16(Console.ReadLine());
+       
+            int greater = numbers(a, b);
+        
+        Console.WriteLine($"a={a}; b={b}");
+        Console.WriteLine($"The Largest Number Is: {greater}");
+        
+    }
+}
+    // Homework Question 2
+    // Managed my time poorly today, could not figure it out timely :(
